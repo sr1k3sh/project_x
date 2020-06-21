a120b2f73434d731adbd863573053e554df29456
rikesh shrestha <sr1k3sh@gmail.com>
2020-06-21 16:43:36 +0545

    Polisingh

--- src/components/FeaturedItems.js
+++ src/components/FeaturedItems.js
@@ -14,11 +14,15 @@ class FeaturedItems extends Component {
                 </div>
                 <div className="featured-item-card flex-row justify-space-between">
                     <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
+                    <FeaturedCard src="./icons/f2.png" price="90,000" details="Iphone XR 256GB" location="Chitwan" date="Jun 18"></FeaturedCard>
+                    <FeaturedCard src="./icons/f3.png" price="50,000" details="Apple watch series 4" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
+                    <FeaturedCard src="./icons/f4.png" price="12,000" details="Pug " location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
                     <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
                     <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
                     <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
-                    <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
-                    <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
+                    <FeaturedCard src="./icons/f2.png" price="90,000" details="Iphone XR 256GB" location="Chitwan" date="Jun 18"></FeaturedCard>
+                    <FeaturedCard src="./icons/f3.png" price="50,000" details="Apple watch series 4" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
+                    <FeaturedCard src="./icons/f4.png" price="12,000" details="Pug " location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
                     <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
                     <FeaturedCard src="./icons/f1.png" price="12,90,000" details="Fiat 500 Convertible Car Leases …" location="Baluwatar, KTM" date="Mar 18"></FeaturedCard>
                 </div>
